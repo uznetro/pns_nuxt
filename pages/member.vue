ec1c7943f1d6b4bda525a2034278f167aa432a4b
uznetro <mayanomikotoba@gmail.com>
2025-03-03 17:37:04 +0900

    feat: member text

--- pages/member.vue
+++ pages/member.vue
@@ -1,11 +1,36 @@
-<script setup></script>
+<!-- <script>
+import Cookies from 'js-cookie';
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
+
+const logout = () => {
+  Cookies.remove('loggedIn');
+  router.push('/login');
+};
+</script> -->
 <template>
   <v-app>
+    <head>
+      <meta name="robots" content="noindex">
+    </head>
+
     <Header />
 
-    <v-main><MemberList /></v-main>
-
+    <v-main>
+      <div class="member-title">
+        <p>ASA Members List</p>
+        <small>The member database is regularly updated.</small>
+      </div>
+      <MemberList />
+    </v-main>
     <Footer />
   </v-app>
 </template>
+
+<style lang="scss">
+.member-title {
+  padding: 20px;
+  background: #e9e9e9;
+}
+</style>
