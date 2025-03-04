2b8d78b2e819778e8ecdd6f968a7e3de4d69b696
uznetro <mayanomikotoba@gmail.com>
2025-03-04 14:35:30 +0900

    fix: member list

---
+++ components/MemberData.js
@@ -0,0 +1,584 @@
+export const headers = [
+  {
+    title: 'Main',
+    align: 'start',
+    sortable: false,
+    key: 'main',
+    width: '160px',
+  },
+  { title: 'HQ', align: 'start', key: 'hq' },
+  { title: 'Type', align: 'start', key: 'type' },
+  { title: 'Mini', align: 'start', key: 'mini' },
+  { title: 'Nation', align: 'start', key: 'nation' },
+]
+
+export　const data = [
+  {
+  "main": "༺ঔৣ Tam★Leঔৣ࿐",
+  "rank": "R5",
+  "hq": 43,
+  "type": "All",
+  "mini": "NA",
+  "nation": "VN"
+  },
+  {
+  "main": "ᶺᶳᶺ Gizmo",
+  "rank": "R4",
+  "hq": 42,
+  "type": "Rider",
+  "mini": "babyGizmo",
+  "nation": "USA"
+  },
+  {
+  "main": "ᶺᶳᶺ SöDA",
+  "rank": "R4",
+  "hq": 43,
+  "type": "Rider",
+  "mini": "ᶺᶳᶺ ɱini SöDA",
+  "nation": "JPN"
+  },
+  {
+  "main": "Dr Baek Kang-hyuk",
+  "rank": "R3",
+  "hq": 43,
+  "type": "S/F",
+  "mini": "NA",
+  "nation": "USA"
+  },
+  {
+  "main": "༺༺ༀ༒Tomo༒ༀ༻ ༻",
+  "rank": "R3",
+  "hq": 43,
+  "type": "All",
+  "mini": "NA",
+  "nation": "VN"
+  },
+  {
+  "main": "ᴺᶠᴳ̷M̷a̷̷v̷",
+  "rank": "R3",
+  "hq": 43,
+  "type": "All",
+  "mini": "barbiequinn",
+  "nation": "USA"
+  },
+  {
+  "main": "殺Fluffy ITALIA戮",
+  "rank": "R3",
+  "hq": 43,
+  "type": "Fighter",
+  "mini": "NA",
+  "nation": "JPN"
+  },
+  {
+  "main": "sᴘᴀᴡɴ",
+  "rank": "R4",
+  "hq": 42,
+  "type": "Shooter",
+  "mini": "Small Spawn",
+  "nation": "USA"
+  },
+  {
+  "main": "❁AzelealoveAsher❁",
+  "rank": "R4",
+  "hq": 42,
+  "type": "Rider",
+  "mini": "❁AzelealoveAsherN❁",
+  "nation": "SG"
+  },
+  {
+  "main": "Rachel Cutter",
+  "rank": "R4",
+  "hq": 41,
+  "type": "Fighter",
+  "mini": "Rachel hacker",
+  "nation": "SG"
+  },
+  {
+  "main": "みんなの♡だいふく",
+  "rank": "R4",
+  "hq": 41,
+  "type": "Fighter",
+  "mini": "大福3150",
+  "nation": "JPN"
+  },
+  {
+  "main": ".¥ Gemini ¥.",
+  "rank": "R3",
+  "hq": 41,
+  "type": "Shooter",
+  "mini": ".¥ MiniG ¥.",
+  "nation": "VN/KOR"
+  },
+  {
+  "main": "Mɾ Sϙυιɠɠʅҽ",
+  "rank": "R3",
+  "hq": 42,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "USA"
+  },
+  {
+  "main": "༺༒lien༒༻",
+  "rank": "R3",
+  "hq": 41,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "JPN"
+  },
+  {
+  "main": "ᶺᶳᶺ Scarlet-y",
+  "rank": "R3",
+  "hq": 41,
+  "type": "Fighter",
+  "mini": "NA",
+  "nation": "JPN"
+  },
+  {
+  "main": "ᶺᶳᶺULIULI",
+  "rank": "R3",
+  "hq": 41,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "JPN"
+  },
+  {
+  "main": "ঔৣ༆Gol☆Kichi༆ঔ",
+  "rank": "R4",
+  "hq": 41,
+  "type": "Fighter",
+  "mini": "mini gol",
+  "nation": "JPN"
+  },
+  {
+  "main": "Brassiere",
+  "rank": "R3",
+  "hq": 40,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "JPN"
+  },
+  {
+  "main": "~Mochito~",
+  "rank": "R4",
+  "hq": 41,
+  "type": "Rider",
+  "mini": "Moch's",
+  "nation": "EURO"
+  },
+  {
+  "main": "༺ༀ.KURUMI.ༀ༻",
+  "rank": "R3",
+  "hq": 40,
+  "type": "Rider",
+  "mini": "kurumiの2号機.༻",
+  "nation": "JPN"
+  },
+  {
+  "main": "༺✠T༒T✠༻",
+  "rank": "R3",
+  "hq": 40,
+  "type": "Rider",
+  "mini": "༺T༒T༻",
+  "nation": "VN"
+  },
+  {
+  "main": "ᶺᶳᶺ pico",
+  "rank": "R3",
+  "hq": 40,
+  "type": "Shooter",
+  "mini": "NA",
+  "nation": "JPN"
+  },
+  {
+  "main": "˚₊̥Säყä˚₊̥",
+  "rank": "R3",
+  "hq": 41,
+  "type": "Rider",
+  "mini": "꒰ঌSena໒꒱",
+  "nation": "JPN"
+  },
+  {
+  "main": "カビゴン★☆",
+  "rank": "R3",
+  "hq": 40,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "JPN"
+  },
+  {
+  "main": "Midnight white",
+  "rank": "R2",
+  "hq": 40,
+  "type": "Rider",
+  "mini": "Mini white",
+  "nation": "NA"
+  },
+  {
+  "main": "WarCmdr40Fighters",
+  "rank": "R1",
+  "hq": 40,
+  "type": "Fighter",
+  "mini": "NA",
+  "nation": "SG"
+  },
+  {
+  "main": "★國泰建設★",
+  "rank": "R1",
+  "hq": 41,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "TW"
+  },
+  {
+  "main": "Λяѕєη",
+  "rank": "R4",
+  "hq": 39,
+  "type": "Rider",
+  "mini": "TBC",
+  "nation": "EURO"
+  },
+  {
+  "main": "༄༂ASA_Huy ༂࿐",
+  "rank": "R4",
+  "hq": 40,
+  "type": "Rider",
+  "mini": "༄༂MiniHuy ༂࿐",
+  "nation": "VN"
+  },
+  {
+  "main": ".༺Diễm Mai ༻.",
+  "rank": "R4",
+  "hq": 39,
+  "type": "Fighter",
+  "mini": "NA",
+  "nation": "VN"
+  },
+  {
+  "main": "Trinity(とりにてぃ)",
+  "rank": "R3",
+  "hq": 39,
+  "type": "Fighter",
+  "mini": "NA",
+  "nation": "JPN"
+  },
+  {
+  "main": "༺ʍαℓ༻",
+  "rank": "R3",
+  "hq": 39,
+  "type": "Rider",
+  "mini": "༺ʍąℓ༻",
+  "nation": "MY"
+  },
+  {
+  "main": "ᶺᶳᶺ マウmaw",
+  "rank": "R3",
+  "hq": 39,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "JPN"
+  },
+  {
+  "main": "•✿•Cẩm Tú•✿•",
+  "rank": "R3",
+  "hq": 39,
+  "type": "Shooter",
+  "mini": "✿•Cẩm Tú•✿",
+  "nation": "VN"
+  },
+  {
+  "main": "⋆☽✶Levina✶☾⋆",
+  "rank": "R3",
+  "hq": 39,
+  "type": "Rider",
+  "mini": "༒OMG༒",
+  "nation": "THAI"
+  },
+  {
+  "main": "Xá xị",
+  "rank": "R4",
+  "hq": 38,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "VN"
+  },
+  {
+  "main": "-Vui Vẻ-",
+  "rank": "R3",
+  "hq": 38,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "VN"
+  },
+  {
+  "main": ".BERLIAN",
+  "rank": "R3",
+  "hq": 39,
+  "type": "Rider",
+  "mini": ".RATU BERLIAN",
+  "nation": "INDO"
+  },
+  {
+  "main": "edkhtan",
+  "rank": "R3",
+  "hq": 38,
+  "type": "Shooter",
+  "mini": "NA",
+  "nation": "MY"
+  },
+  {
+  "main": "TuấnPhương88",
+  "rank": "R3",
+  "hq": 38,
+  "type": "Shooter",
+  "mini": "NA",
+  "nation": "JPN"
+  },
+  {
+  "main": "TínhGià",
+  "rank": "R3",
+  "hq": 38,
+  "type": "Fighter",
+  "mini": "TínhGià.Mini",
+  "nation": "VN"
+  },
+  {
+  "main": "Zombie Slayer ♡",
+  "rank": "R3",
+  "hq": 39,
+  "type": "Rider",
+  "mini": "༺lil zom༻",
+  "nation": "USA"
+  },
+  {
+  "main": "༄ঔ༒sunny༒ঔ࿐",
+  "rank": "R3",
+  "hq": 38,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "JPN"
+  },
+  {
+  "main": "ཌༀ۝VICTOR۝ༀད",
+  "rank": "R3",
+  "hq": 38,
+  "type": "Fighter",
+  "mini": "༺ MILO ༻",
+  "nation": "THAI"
+  },
+  {
+  "main": "ᶺᶳᶺ Thanh Nguyễn",
+  "rank": "R3",
+  "hq": 39,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "VN"
+  },
+  {
+  "main": "ᶺᶳᶺ Zen_ou",
+  "rank": "R3",
+  "hq": 38,
+  "type": "Fighter",
+  "mini": "NA",
+  "nation": "JPN"
+  },
+  {
+  "main": "KAIDO",
+  "rank": "R3",
+  "hq": 38,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "INDO"
+  },
+  {
+  "main": "★ᴍʀ ᴅ ᴀ ʀ ᴋ★",
+  "rank": "R3",
+  "hq": 38,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "VN"
+  },
+  {
+  "main": "☆Itto~_~Coffe☆",
+  "rank": "R3",
+  "hq": 38,
+  "type": "Rider",
+  "mini": "☆Itooコーヒー☆",
+  "nation": "VN/JPN"
+  },
+  {
+  "main": "☞╯ƙє√ɨɲ︵☆",
+  "rank": "R3",
+  "hq": 38,
+  "type": "Shooter",
+  "mini": "~Kevin~",
+  "nation": "VN"
+  },
+  {
+  "main": "✯꧁MinioN꧂✯",
+  "rank": "R3",
+  "hq": 40,
+  "type": "Rider",
+  "mini": "➷Minion 2.0 ᥫ᭡",
+  "nation": "TBC"
+  },
+  {
+  "main": "廴ᴋᴇɴᴾᴿᴼシ404",
+  "rank": "R3",
+  "hq": 38,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "VN"
+  },
+  {
+  "main": "꧁༺ LÊ TÙNG ꧂",
+  "rank": "R3",
+  "hq": 38,
+  "type": "Fighter",
+  "mini": "Lê Tùng 9",
+  "nation": "VN"
+  },
+  {
+  "main": "Haram ✧˖°. ♪",
+  "rank": "R3",
+  "hq": 39,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": ""
+  },
+  {
+  "main": "ᶺᶳᶺÐařŧhŽaža",
+  "rank": "R3",
+  "hq": 37,
+  "type": "Rider",
+  "mini": "ᶺᶳᶺÐařŧhŽořo",
+  "nation": "VN"
+  },
+  {
+  "main": "Acid Black Cherry♡",
+  "rank": "R3",
+  "hq": 36,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "JPN"
+  },
+  {
+  "main": "꧁༺AFK༻꧂.",
+  "rank": "R3",
+  "hq": 36,
+  "type": "Shooter",
+  "mini": "꧁༺. BụIcÁm.༻꧂",
+  "nation": "VN"
+  },
+  {
+  "main": "βaby SöDA",
+  "rank": "R1",
+  "hq": 31,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "JPN"
+  },
+  {
+  "main": "sain❁ty",
+  "rank": "R2",
+  "hq": 30,
+  "type": "Shooter",
+  "mini": "NA",
+  "nation": "USA"
+  },
+  {
+  "main": "ᶺᶳᶺ NARCISO",
+  "rank": "R3",
+  "hq": 29,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "EURO"
+  },
+  {
+  "main": "Bυttʅҽʂtιƈƙʂ",
+  "rank": "R2",
+  "hq": 29,
+  "type": "Fighter",
+  "mini": "NA",
+  "nation": "SG"
+  },
+  {
+  "main": "Dαყɱαn",
+  "rank": "R2",
+  "hq": 28,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "EURO"
+  },
+  {
+  "main": "◣_◢ Panda_ꓘſ",
+  "rank": "R3",
+  "hq": 27,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "TBC"
+  },
+  {
+  "main": "NgọcDuySàiGòn",
+  "rank": "R2",
+  "hq": 27,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "VN"
+  },
+  {
+  "main": "༺Ċherry༻",
+  "rank": "R2",
+  "hq": 27,
+  "type": "Shooter",
+  "mini": "NA",
+  "nation": "EURO"
+  },
+  {
+  "main": "вαιт",
+  "rank": "R2",
+  "hq": 26,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "SG"
+  },
+  {
+  "main": "༺❀Tâm的小弟❀༻",
+  "rank": "R1",
+  "hq": 25,
+  "type": "Fighter",
+  "mini": "NA",
+  "nation": "SG"
+  },
+  {
+  "main": "SP…",
+  "rank": "R3",
+  "hq": 24,
+  "type": "Shooter",
+  "mini": "NA",
+  "nation": "VN"
+  },
+  {
+  "main": "Clear",
+  "rank": "R2",
+  "hq": 24,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "KOR"
+  },
+  {
+  "main": "༺ʍǟℓ༻",
+  "rank": "R1",
+  "hq": 24,
+  "type": "Rider",
+  "mini": "NA",
+  "nation": "TW"
+  },
+  {
+  "main": "Princess",
+  "rank": "R1",
+  "hq": 19,
+  "type": "Fighter",
+  "mini": "NA",
+  "nation": "USA"
+  }
+  ]
